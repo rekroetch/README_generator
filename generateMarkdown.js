24485345586f9b384719c51190fc8ad78af8babd
Rachel Kroetch <rekroetch@ucdavis.edu>
2020-12-01 17:01:19 -0800

    basic layout

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,31 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-  
+    
+    ## Description
+    ${data.description}
+
+    ## Table of Contents
+    [Installation](#installation)
+    [Usage](#usage)
+    [Tests](#tests)
+    [Contributing](#contributing)
+    [License](#license)
+
+    ## Installation
+    Use ${data.installation} to install dependencies.
+
+    ## Usage
+    ${data.usage}
+
+    ## Tests
+    Use ${data.tests} to run tests.
+
+    ## Contributing
+    ${data.contributing}
+
+    ## License
+    [${data.license}](https://opensource.org/licenses/${data.license})
   `;
   }
   
